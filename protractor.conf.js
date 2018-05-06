1bda2a39bd5188fdd50d7910672828865ad938b0
king4kings <anandkingsly@gmail.com>
2018-05-06 11:56:25 +0530

    Modified the base URL

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://ezgo-web.herokuapp.com/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
